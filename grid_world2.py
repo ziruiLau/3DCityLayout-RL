70618f41e7fda00acfb989d4a834f89610bcbc23
zirui <835281037@qq.com>
2022-07-15 16:26:40 -0400

    [comment] commit

--- grid_world2.py
+++ grid_world2.py
@@ -28,27 +28,6 @@ class RotatedRect:
     def intersection(self, other):
         return self.get_contour().intersection(other.get_contour())
 
-# calculate iou
-import shapely.geometry
-import shapely.affinity
-class RotatedRect:
-    def __init__(self, cx, cy, w, h, angle):
-        self.cx = cx
-        self.cy = cy
-        self.w = w
-        self.h = h
-        self.angle = angle
-
-    def get_contour(self):
-        w = self.w
-        h = self.h
-        c = shapely.geometry.box(-w/2.0, -h/2.0, w/2.0, h/2.0)
-        rc = shapely.affinity.rotate(c, self.angle)
-        return shapely.affinity.translate(rc, self.cx, self.cy)
-
-    def intersection(self, other):
-        return self.get_contour().intersection(other.get_contour())
-
     def area(self):
         return self.get_contour().area
 
